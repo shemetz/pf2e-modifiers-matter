bd22843967772d5a8beec5ee6f5dd777fcaafe33
itamar <itamarcu@gmail.com>
2026-01-18 18:46:26 +0200

    Fixed getDocFromUuidSync utility function

--- scripts/pf2emm-utils.mjs
+++ scripts/pf2emm-utils.mjs
@@ -11,8 +11,11 @@ export const getSetting = (settingName) => game.settings.get(MODULE_ID, settingN
 
 export const getDocFromUuidSync = (uuid) => {
   if (mocks.fromUuidSync_mock)
-    return mocks.fromUuidSync_mock[uuid] ?? Error(`No mock found for UUID ${uuid}`)
-  else
-    return fromUuidSync(uuid)
+    if (mocks.fromUuidSync_mock[uuid])
+      return mocks.fromUuidSync_mock[uuid]
+    else
+      throw Error(`No mock found for UUID ${uuid}`)
+
+  return fromUuidSync(uuid)
 }
 
